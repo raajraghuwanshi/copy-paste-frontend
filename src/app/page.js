78e39de26fbe56b427d490ae400271ffb1f7145b
raj raghuwanshi <rajraghuawnshi9479@gmail.com>
2026-05-07 21:09:14 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+const page = () => {
+  const [first, setfirst] = useState('')
+  function send(){
+
+  }
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
